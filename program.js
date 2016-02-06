6f745c66755d392da53de2cca5ee077438daf878
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-02-05 20:47:40 -0600

    first async I/O

--- program.js
+++ program.js
@@ -1,33 +1,16 @@
-//Write a program that uses a single synchronous filesystem operation to
-  //read a file and print the number of newlines (\n) it contains to the
-  //console (stdout), similar to running cat file | wc -l.
+//## MY FIRST ASYNC I/O! (Exercise 4 of 13)
 
-  //The full path to the file to read will be provided as the first
-  //command-line argument (i.e., process.argv[2]). You do not need to make
-  //your own test file.
-//
+
 var fs = require('fs');
 
-var contents = fs.readFileSync(process.argv[2]);
-//var contents = fs.readFileSync(process.argv[2], 'utf8');
+var file = process.argv[2];
 
-var lines = contents.toString().split('\n').length - 1;
-//var lines = contents.split('\n').length - 1;
+fs.readFile(file, function (err, contents) {
+// fs.readFile(file, 'utf8', callback) can also be used
 
-// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1;
+  var lines = contents.toString().split('\n').length - 1;
 
-console.log(lines)
+  console.log(lines);
 
+});
 
-
-/// OFFICIAL SOLUTION ///
-
-var fs = require('fs')
-
- var contents = fs.readFileSync(process.argv[2])
- var lines = contents.toString().split('\n').length - 1
- console.log(lines)
-
-// note you can avoid the .toString() by passing 'utf8' as the
-     // second argument to readFileSync, then you'll get a String!
-     // fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
